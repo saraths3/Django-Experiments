88505f3d852cb9d776c14dea5cb4b6bfe6d4eda9
SARATH S <sarathpvtmail@gmail.com>
2026-05-13 14:46:50 +0530

    Add files via upload

--- Django Experiments/mysite2/produts/admin.py
+++ Django Experiments/mysite2/produts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Employee, Teachers, MyStudents, userdata
+
+admin.site.register(Employee)
+admin.site.register(Teachers)
+admin.site.register(MyStudents)
+admin.site.register(userdata)
--- Django Experiments/mysite2/produts/models.py
+++ Django Experiments/mysite2/produts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name;self.age = age
+
+# s1 = Student('Sarath S', 22)
+
+class MyStudents(models.Model):
+    name = models.CharField(max_length = 20)
+    roll = models.IntegerField()
+    address = models.TextField() 
+    bio = models.TextField(default='Student of University of Kerala')
+
+class Teachers(models.Model):
+    name = models.CharField(max_length = 100)
+    phone = models.IntegerField()
+    email = models.EmailField()
+
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    discription = models.TextField()
+    salary = models.IntegerField()
+
+
+class userdata(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    address = models.TextField()
+    email = models.EmailField()
+    
